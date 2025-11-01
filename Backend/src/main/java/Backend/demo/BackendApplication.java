7f74888b59a5af982e52483e299fc029326ed9c0
AlikBook <ivanarturograndi6@gmail.com>
2025-11-01 08:48:32 +0100

    Changed the name of root file

--- Backend/src/main/java/Backend/demo/BackendApplication.java
+++ Backend/src/main/java/Backend/demo/BackendApplication.java
@@ -1,13 +1,13 @@
-package REST_API.demo;
+package Backend.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApiApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestApiApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
--- Backend/src/test/java/Backend/demo/BackendApplicationTests.java
+++ Backend/src/test/java/Backend/demo/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package REST_API.demo;
+package Backend.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestApiApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
